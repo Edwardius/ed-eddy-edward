fd5362d771ac62537a12e87d5b0ab4de3ff1033d
Eddy Zhou <eddy@sanctuary.ai>
2022-11-07 12:13:50 -0500

    deconstructing reference website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme =
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +10,77 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    // Breakpoints which determine what css layout should be 
+    // used for each screen size (eg. "w-16 md:w-32 lg:w-48")
+    screens: {
+      'sm': '640px', 
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    // Default Colour Palette
+    colors: {
+      transparent: 'trnsparent',
+      gray: colors.gray,
+      'olive': '#ebe7e0',
+      'azukired': '#c03541'
+    },
+    fontWeight: {
+      '100': '100',
+      '200': '200',
+      '300': '300',
+      '400': '400',
+      '500': '500',
+      '600': '600',
+      '700': '700',
+      '800': '800',
+      '900': '900',
+    },
+    // Spacing scale, can override these values, new keys must be place in extend.spacing
+    spacing: {
+      px: '1px',
+      0: '0',
+      0.5: '0.125rem',
+      1: '0.25rem',
+      1.5: '0.375rem',
+      2: '0.5rem',
+      2.5: '0.625rem',
+      3: '0.75rem',
+      3.5: '0.875rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      28: '7rem',
+      32: '8rem',
+      36: '9rem',
+      40: '10rem',
+      44: '11rem',
+      48: '12rem',
+      52: '13rem',
+      56: '14rem',
+      60: '15rem',
+      64: '16rem',
+      72: '18rem',
+      80: '20rem',
+      96: '24rem',
+    },
+    extend: {
+      fontFamily: {
+        'mono': ['Mono', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans'],
+        'title': ['MSBee', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans']
+      }
+    },
   },
   plugins: [],
 }
