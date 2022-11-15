58999f70c03f6f40775c6e3de9fa771689054ff6
Eddy Zhou <eddy@sanctuary.ai>
2022-11-15 01:54:54 -0500

    phantom buttons

--- components/nav/nav_hamburger/menu.tsx
+++ components/nav/nav_hamburger/menu.tsx
@@ -7,13 +7,13 @@ export default function Menu ({open, active_page}:
     
     return (
       <div className={classNames('pt-20 px-6 z-20 fixed transition h-screen w-screen ease-in backdrop-blur-sm bg-gray-100 \
-      bg-opacity-60', {'opacity-0': !open})}>
+      bg-opacity-60', {'opacity-0 -z-10': !open})}>
         <ul className='w-full'>
-          <li><GenericItem text='Connect' href='/connect' active={active_page}/></li>
-          <li><GenericItem text='Mindmap' href='/mind_map' active={active_page}/></li>
-          <li><GenericItem text='Projects' href='/projects' active={active_page}/></li>
-          <li><GenericItem text='Leadership' href='/leadership' active={active_page}/></li>
-          <li><GenericItem text='Future' href='/future' active={active_page}/></li>
+          <li><GenericItem text='Connect' href='/connect' active={active_page} open={open}/></li>
+          <li><GenericItem text='Mindmap' href='/mind_map' active={active_page} open={open}/></li>
+          <li><GenericItem text='Projects' href='/projects' active={active_page} open={open}/></li>
+          <li><GenericItem text='Leadership' href='/leadership' active={active_page} open={open}/></li>
+          <li><GenericItem text='Future' href='/future' active={active_page} open={open}/></li>
           
         </ul>
       </div>
--- components/nav/nav_hamburger/menu_items/generic_item.tsx
+++ components/nav/nav_hamburger/menu_items/generic_item.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 import classNames from 'classnames'
 
-export default function GenericItem({ text, href, active }: 
-  { text: string, href: string, active: string }) {
+export default function GenericItem({ text, href, active, open }: 
+  { text: string, href: string, active: string, open: boolean}) {
 
   return(
     <Link href={href}>
       <button className={classNames('pt-6 pb-2 w-full uppercase font-900 text-2xl \
       text-left font-title border-b-2 border-gray-800 border-opacity-20', {'text-gray-800' : active != text}, 
-      {'text-azukired-500': active == text})}>
+      {'text-azukired-500': active == text})} disabled={!open}>
           {text}
       </button>
     </Link> 
