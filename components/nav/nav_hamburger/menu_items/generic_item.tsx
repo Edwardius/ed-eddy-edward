edd75fcc7c8807a9dd25d812a550f9c867090701
Eddy Zhou <eddy@sanctuary.ai>
2022-11-15 19:55:21 -0500

    test

--- components/nav/nav_hamburger/menu_items/generic_item.tsx
+++ components/nav/nav_hamburger/menu_items/generic_item.tsx
@@ -2,15 +2,32 @@ import React from 'react'
 import Link from 'next/link'
 import classNames from 'classnames'
 
-export default function GenericItem({ text, href, active, open }: 
-  { text: string, href: string, active: string, open: boolean}) {
+interface ButtonProps {
+  text: string,
+  href: string, 
+  active: string,
+  open: boolean, 
+  icon?: string, 
+  red?: boolean
+}
+
+export default function GenericItem({text, href, active, open, icon = '', red = false}: ButtonProps) {
+  let icon_ele;
+  
+  if (icon) {
+    icon_ele = <img src={icon} className='translate-y-1.5 -translate-x-2 float-right right-3 h-4'/>;
+  }
+  else {
+    icon_ele = null;
+  }
 
   return(
     <Link href={href}>
-      <button className={classNames('pt-6 pb-2 w-full uppercase font-900 text-2xl \
-      text-left font-title border-b-2 border-gray-800 border-opacity-20', {'text-gray-800' : active != text}, 
-      {'text-azukired-500': active == text})} disabled={!open}>
+      <button className={classNames('transition ease-in pt-6 pb-2 w-full uppercase font-900 text-2xl \
+      text-left font-title border-b-2 border-gray-800 border-opacity-20', {'hover:text-azukired-500 text-gray-800' : active != text}, 
+      {'text-azukired-500': red}, {'text-gray-500 text-opacity-50': active == text})} disabled={!open || active == text}>
           {text}
+          {icon_ele}
       </button>
     </Link> 
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import Head from 'next/head'
 import NavBar from '../components/nav/nav_bar'
+import getBreakpoint from '../components/utils/get_breakpoint';
+
+import getWindowDimensions from '../hooks/window_size'
 
 export default function Home() {
+  const size = getWindowDimensions();
+  const breakpoint = getBreakpoint('xl');
+  let background;
+
+  if (size.width == null) {
+    background = <img src='eee-homepage.png' className='w-full block h-screen object-cover object-center overlay-item z-0 bottom-0'/>
+  }
+  else if (size.width < breakpoint) {
+    background = <img src='eee-homepage.png' className='w-full block h-screen object-cover object-center overlay-item z-0 bottom-0'/>
+  }
+  else {
+    background = <video className='w-full block h-screen object-cover object-center overlay-item z-0 bottom-0' muted loop autoPlay playsInline>
+        <source src='/eee-animation.mp4' type="video/mp4"/>
+      </video>
+  }
+
   
   return (
     <div>
@@ -14,14 +33,11 @@ export default function Home() {
       <NavBar active_page={Home.name}/>
       
       <div>
-        <video className='w-full block h-screen object-cover object-center overlay-item z-0 bottom-0' muted loop autoPlay playsInline>
-          <source src='/eee-animation.mp4' type="video/mp4"/>
-        </video>
+        {background}
 
         <img src='/artist-touch.png' className='absolute z-10 md:bottom-20 md:right-20 md:w-64 \
         xs:right-10 xs:bottom-10 xs:w-32'/>
       </div>
-
       {/* End Main of Page */}
 
     </div>
