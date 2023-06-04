29cfdee3cdb539ceb41232d46822c5d1d6415549
Eddy Zhou <eddy@sanctuary.ai>
2023-06-04 02:22:48 -0400

    minor fixes and icon

--- components/nav/nav_hamburger/menu_items/wato_item.tsx
+++ components/nav/nav_hamburger/menu_items/wato_item.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 
 
-export default function WATOItem({open}: {open:boolean}) {
+export default function WATOItem() {
 
   return(
     <Link href='https://www.watonomous.ca/'>
       <button className='transition-colors ease-in \
-      translate-y-0 w-full pt-6 pb-2 border-b-2 border-gray-800 border-opacity-20' disabled={!open}>
+      translate-y-0 w-full pt-6 pb-2 border-b-2 border-gray-800 border-opacity-20'>
           <img src='/wato-logo.png' className='h-10 hover:opacity-60'></img>
       </button>
     </Link> 
