582c9470d0c9cb052bbe302f53d274bb5b8fc389
Eddy Zhou <edwardius@Eddys-MacBook-Air.local>
2023-09-18 00:46:06 -0600

    nvm, i dont like the blurring

--- components/nav/nav_desktop/nav_buttons/nav_button.tsx
+++ components/nav/nav_desktop/nav_buttons/nav_button.tsx
@@ -7,7 +7,7 @@ export default function NavButton({ text, href, active }:
   
   return(
     <Link href={href}>
-      <button className={classNames('transition-colors ease-in backdrop-blur-xs bg-black bg-opacity-5 hover:bg-opacity-10 \
+      <button className={classNames('transition-colors ease-in bg-black bg-opacity-3 hover:bg-opacity-10 \
     py-1.5 px-4 rounded text-xs uppercase font-mono font-100', {'underline font-400': active == text})}>
         {text}
       </button>
--- components/pop_ups/connect/connect_button.tsx
+++ components/pop_ups/connect/connect_button.tsx
@@ -5,7 +5,7 @@ export default function ConnectButton ({connect_open, setOpen}:
   {connect_open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>>}) {
 
   return(
-    <button className={classNames('transition-colors backdrop-blur-xs ease-in bg-black bg-opacity-3 hover:bg-opacity-10 \
+    <button className={classNames('transition-colors ease-in bg-black bg-opacity-3 hover:bg-opacity-10 \
             py-1.5 px-4 rounded text-xs uppercase font-mono font-100')} onClick={() => setOpen(!connect_open)}>
         Connect
     </button>
