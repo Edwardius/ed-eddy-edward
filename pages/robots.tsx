0b1185c3b4d7729111bfbe8b40bd99015b41672b
Eddy Zhou <edsteredward@gmail.com>
2024-12-24 21:55:25 -0800

    gotta direct buttons to the right pages

--- pages/robots.tsx
+++ pages/robots.tsx
@@ -3,19 +3,19 @@ import NavBar from '../components/nav/nav_bar'
 
 import useLockedBody from '../hooks/lock_scroll'
 
-export default function Epiphanies() {
+export default function Robots() {
   const [locked, setLocked] = useLockedBody(false, 'root')
   return (
     <div>
       <Head>
-        <title>EdEddyEdward | Epiphanies</title>
+        <title>EdEddyEdward | Robots</title>
         <link rel='icon' href='/icon.ico'/>
       </Head>
 
       {/* Begin Main of Page */}
       <main className='bg-azukigray'>
         {/* Navigation Bar */}
-        <NavBar active_page='Epiphanies' setLocked={setLocked}/>
+        <NavBar active_page='Robots' setLocked={setLocked}/>
 
         {/* Body */}
         <div className='px-4 pt-24 pb-96'>
